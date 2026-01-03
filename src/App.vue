5453deb219291ac436423b997ac1df394000962c
mugni1 <asepabdulmugni22@gmail.com>
2026-01-03 18:06:37 +0700

    reafactor: improve

--- src/App.vue
+++ src/App.vue
@@ -18,31 +18,27 @@
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header
-          class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
-        >
+        <header class="flex h-12 items-center gap-2 fixed w-full border-b bg-white z-50">
           <div class="flex items-center gap-2 px-4">
             <SidebarTrigger class="-ml-1" />
             <Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
             <Breadcrumb>
               <BreadcrumbList>
-                <BreadcrumbItem class="hidden md:block">
+                <BreadcrumbItem class="block">
                   <BreadcrumbLink class="capitalize">
                     <RouterLink :to="$route.path">
                       {{ $route.name }}
                     </RouterLink>
                   </BreadcrumbLink>
                 </BreadcrumbItem>
-                <!-- <BreadcrumbSeparator class="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem> -->
               </BreadcrumbList>
             </Breadcrumb>
           </div>
         </header>
-        <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <RouterView />
+        <div class="flex flex-col gap-4 p-4 w-full overflow-auto pt-16">
+          <div class="w-full h-full space-y-4 overflow-x-auto">
+            <RouterView />
+          </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
